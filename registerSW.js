46cb467ded2bccc73ff549607811a6fc9682ab14
leafaroinafipe <leafar.oinafipe@gmail.com>
2026-05-01 16:10:57 -0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Cottolengo_Escala_Local/sw.js', { scope: '/Cottolengo_Escala_Local/' })})}
